7a63fcf4b1f58347cc8b351f2bbda936d293240e
Maria Rodriguez <mooki593@gmail.com>
2021-11-05 17:48:15 -0600

    Started characters page

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -1,3 +1,16 @@
-import { films } from '../data/films' 
+import { films } from "../data/films.js";
 
-console.log("Hello World")
+let filmlist = document.querySelector("#filmList");
+
+for (let i = 0; i < films.length; i++) {
+  let figure = document.createElement("figure");
+  let figImg = document.createElement("img");
+  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i+1}.jpg`;
+  let figCaption = document.createElement("figcaption");
+  figCaption.textContent = films[i].title
+
+  figure.appendChild(figImg);
+  figure.appendChild(figCaption);
+
+  filmlist.appendChild(figure);
+}
