4a7c57ed07ea3f29bc13e67961190da5e47f2e6d
Maria Rodriguez <mooki593@gmail.com>
2021-12-03 02:12:36 -0700

    added buttons and added 25 pokemon flipped cards

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -5,17 +5,43 @@
     console.error(error);
   }
 }
-
-getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`)
+function loadPokemon(offset = 0, limit = 25) {
+getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
 .then((data) => {
   console.log(data);
   for (const pokemon of data.results) {
       console.log(pokemon)
-       getAPIData(pokemon.url).then(pokeData => populatePokeCards(pokeData));
-  }
-});
+       getAPIData(pokemon.url).then((pokeData) => populatePokeCards(pokeData), 
+       )
+};
+}
+)
+}
 
 const pokeGrid = document.querySelector(".pokeGrid");
+const loadButton = document.querySelector('.loadPokemon')
+loadButton.addEventListener('click', () => loadPokemon(100,5))
+
+const newButton = document.querySelector('.newPokemon')
+newButton.addEventListener('click', () => {
+    let pokeName = prompt('What is the name of your new Pokemon?')
+    let pokeHeight = prompt("What is the Pokemon's height?")
+    let pokeWeight = prompt("What is the Pokemon's weight?")
+    let newPokemon = new Pokemon(pokeName, pokeHeight, pokeWeight, [])
+populatePokeCard(newPokemon)
+})
+
+class Pokemon {
+    constructor(name, height, weight, abilities) {
+        this.id = 100,
+        this.name = name, 
+        this.height= height,
+        this.weight = weight
+        this.abilities = abilities
+    }
+}
+
+
 
 function populatePokeCards(singlePokemon) {
   const pokeScene = document.createElement("div");
